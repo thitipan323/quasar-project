e0726b21467e76b61e2f770d7d4d6e12919d11e5
thitipan323 <akitokung71@gmail.com>
2023-10-13 14:01:26 +0700

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+  </div>
+  <InputPage/>
+  <FunctionPage/>
+</template>
+
+
+<script>
+
+import InputPage from 'src/pages/InputPage.vue';
+import FunctionPage from 'src/pages/FunctionPage.vue';
+
+export default {
+  name: 'HtmlComponent',
+    components:{
+      InputPage,
+      FunctionPage
+    },
+      
+  data() {
+    return {
+      title: 'thitipan jaisin.',
+    }
+  },
+}
+</script>
+
+<style scoped></style>
